d5042575b60dc6ecfae32eb3c30ca1bad3cb0e61
Gulyaev Martinas <killfind@yandex.ru>
2024-07-22 23:45:03 +0300

    Функции и дз

---
+++ module_2_5.py
@@ -0,0 +1,16 @@
+def get_matrix(n,m,value):
+    matrix  = []
+    #для кол-ва строк матрицы, n повторов
+    for i in range(n):
+        matrix.append([])
+
+        #for для кол - ва столбцов матрицы, m повторов и пополняю список.
+        for j in range(m):
+            matrix[i].append(value)
+
+    print(matrix)
+result1 = get_matrix(2,2,10)
+
+print(result1)
+
+
