14992e3dcf033edc01e3d0d09546d52b64a2377b
Gulyaev Martinas <killfind@yandex.ru>
2024-08-05 21:44:39 +0300

    модули

---
+++ module1.py
@@ -0,0 +1,6 @@
+from module2 import say_hy as sh
+print("Привет  я из модуля один")
+def sh():
+    print('1')
+
+sh()
---
+++ module2.py
@@ -0,0 +1,9 @@
+def say_hy():
+    print("привет я из функции во втром модуле")
+def main():
+    a = 5
+    b = 10
+    print("Привет")
+
+if __name__ == '__main__':
+    main()
