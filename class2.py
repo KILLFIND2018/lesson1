88446da66b4eff375b2d45c7961a4d112952b500
Gulyaev Martinas <killfind@yandex.ru>
2024-10-24 21:20:35 +0300

    перегрузка методов

--- class2.py
+++ class2.py
@@ -1,3 +1,6 @@
+from fsspec.utils import other_paths
+
+
 class Human:
     def __init__(self, name, age):
         #объект и это как пространство имен
@@ -8,6 +11,25 @@ class Human:
     def  __len__(self):
         return self.age
 
+    def __lt__(self, other):
+        #если меньше
+        return self.age < other.age
+    #оператор больше
+    def __gt__(self, other):
+        return self.age > other.age
+    #равно
+    def __eq__(self, other):
+        return self.age == other.age
+    # истина или ложь
+    def __bool__(self):
+        return bool(self.age)
+    #отображение в виде строки
+    def __str__(self):
+        return f'{self.name}'
 
 den = Human('den', 22)
-print(len(den))
+max = Human('max',23)
+print(den < max)
+print(den>max)
+print(den == max)
+print(den)
